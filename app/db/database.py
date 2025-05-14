e8c4f84a3116a80525e0f2f2df68ce4fc0dcc343
Enrique de la Brena Sevillano <enrique.de_la_brena_sevillano@mercedes-benz.com>
2025-05-14 07:51:42 +0200

    feat: add dotenv

--- app/db/database.py
+++ app/db/database.py
@@ -1,12 +1,15 @@
+from dotenv import load_dotenv
 import os
 from sqlmodel import SQLModel, create_engine, Session
 
+load_dotenv()
+
 # Construct DATABASE_URL from individual environment variables
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "task_manager")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
